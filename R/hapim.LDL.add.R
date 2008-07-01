fda824ddeb8b59bbece2977865352ab0edddfdf7
Brigitte Mangin <brigitte.mangin@toulouse.inra.fr>
2008-07-01 00:00:00 +0000

    version 1.0

---
+++ R/hapim.LDL.add.R
@@ -0,0 +1,249 @@
+`hapim.LDL.add` <-
+function(hap.chrom1.pere,hap.chrom2.pere,hap.trans.mere,perf,CD,genea,PLA,map,position,temps.depart,perfectLD,marq.hap.left){
+
+if (perfectLD!=TRUE) stop("error",call.=FALSE) 
+
+# rangement de perf,CD,PLA,hap.trans.mere,hap.chrom1.pere,hap.chrom2.pere en fonction de l'index des pères (2nde colonne de genea)
+##################################################################################################################################
+#on range perf et CD en fonction de l'index des pères
+	temp=cbind(genea,perf,CD)
+	a=temp[order(temp[,2]),]
+	genea=a[,1:2]
+	perf=a[,3]
+	CD=a[,4]
+
+#on range hap.trans.mere en fonction de l'index des pères
+	temp2=cbind(genea,hap.trans.mere)
+	a2=temp2[order(temp2[,2]),]
+	hap.trans.mere=a2[,-c(1:2)]
+
+#on range hap.chrom1.pere en fonction de l'index des pères
+	genea.unique=unique(temp[,2])
+	temp3=cbind(genea.unique,hap.chrom1.pere)
+	a3=temp3[order(temp3[,1]),]
+	hap.chrom1.pere=a3[,-1]
+
+#on range hap.chrom2.pere en fonction de l'index des pères
+	temp4=cbind(genea.unique,hap.chrom2.pere)
+	a4=temp4[order(temp4[,1]),]
+	hap.chrom2.pere=a4[,-1]
+
+# recodage des haplotypes
+#########################
+	 hap.pop	=	rbind(hap.chrom1.pere,hap.chrom2.pere,hap.trans.mere)
+	 all.marq 	= 	allele.marq(hap.pop)
+	 hap.chrom1.pere= 	recode.hap(hap.chrom1.pere,all.marq)
+	 hap.chrom2.pere= 	recode.hap(hap.chrom2.pere,all.marq)
+	 hap.trans.mere = 	recode.hap(hap.trans.mere,all.marq)
+	 hap.pop	=	rbind(hap.chrom1.pere,hap.chrom2.pere,hap.trans.mere)
+ 
+# calcul du nombre d'intervalles
+################################
+	 nbre.int	=	length(map) 
+
+
+# calcul du nombre de marques de l'haplotype: même nbre de marques à droite et à gauche de la position de test
+##############################################################################################################
+	 marq.hap	=	2*marq.hap.left
+
+
+# distance des marques par rapport à l'origine
+##############################################
+	 dist.marq 	= 	distance.marqueurs(map)
+
+
+#Contrôle sur les positions de tests et redimensionnement du vecteur de positions de tests en fonction des positions cohérentes
+###############################################################################################################################
+# 1ère vérification: les positions rentrées par l'utilisateur doivent être comprises entre la 1ère marque et la dernière marque
+# Les positions sont dans position.new
+         position=sort(position) #rangement des valeurs de position par ordre croissant
+         position=round(position,5) #Il faut arrondir!
+
+         if (marq.hap.left==1) {dist.marq1=dist.marq}
+         if (marq.hap.left>1) {dist.marq1=dist.marq[-c(1:(marq.hap.left-1),(length(dist.marq)-marq.hap.left+2):length(dist.marq))]}
+
+         borne.inf=round(dist.marq1[1],5) #Il faut arrondir!
+         borne.sup=round(dist.marq1[length(dist.marq1)],5) #Il faut arrondir!
+
+
+         diff.left=position-borne.inf 
+         diff.left
+         diff.right=position-borne.sup 
+         diff.right
+
+         which=(1:length(position))[(diff.left>=0)&(diff.right<=0)]
+         position.new=round(sort(position[which]),5)
+         position.new
+
+         if ((length(position.new)-marq.hap.left+1)!=length(position)) stop("error in test positions",call.=FALSE) 
+
+# 2nde vérification:les positions rentrées par l'utilisateur doivent être cohérentes avec les colonnes de PLA 
+        if  (dim(PLA)[2]!=length(position.new)) stop("the number of PLA positions is not consistent with the number of test positions",call.=FALSE)
+
+
+# calcul du nombre de positions de tests
+########################################
+	num.pos		=	length(position.new)
+
+# distance des position de tests par rapport à l'origine
+########################################################
+        dist.test 	= 	distance.test(position.new,dist.marq)
+
+
+# frequence allelique
+#####################
+	freq.a        	= freq.all(hap.pop)
+
+
+# calcul de la vraisemblance sous H0 (pas de QTL)
+#################################################
+	 desc.pere     = 	descendant.pere(genea)
+	 moyenne.pere  = 	moyenne.pere(perf,CD,desc.pere)
+	 ML.H0         = 	obj.LDL.add.H0(moyenne.pere,perf,CD,desc.pere)
+
+
+# Boucle
+########
+# Initialisation des vecteurs/matrices résultats:LRT,pos.test,assoc.est,hap.ass.est,param.est
+	 LRT		= 	rep(NA,num.pos)
+	 pos.test  	= 	rep(NA,num.pos)
+	 assoc.est 	= 	rep(NA,num.pos)
+	 hap.ass.est	= 	rep(NA,num.pos)
+	 if(perfectLD==TRUE){
+	      param.est = matrix(NA,ncol=num.pos,nrow=5)
+	 }
+	 if(perfectLD==FALSE){
+	      param.est = matrix(NA,ncol=num.pos,nrow=6)
+	 }
+
+# Initialisation de l'index comptant les positions de test
+ 	i.pos	=	0
+
+# Initialisation de l'index comptant le nbre de colonnes à passer dans proba_de_liaison
+ 	i.pos.PLA	=	0
+
+
+  if(marq.hap.left>1) {
+
+      for(i in 1:(marq.hap.left-1)) {   
+
+         nbre.pas	=	length(dist.test[[i]])
+         i.pos.PLA	=	i.pos.PLA    +   nbre.pas
+
+      }
+   } 
+
+
+    for (i in marq.hap.left:(nbre.int-marq.hap.left+1)) { # Début de la boucle sur les intervalles (FOR1)
+
+    #extraction des informations pour l'intervalle i
+    nbre.all.marq	=	rep(0,marq.hap)
+    freq.marq		=	list()
+    all.marq.int	=	list()
+
+        for(im in 1:(marq.hap)){ # Début FOR2
+             nbre.all.marq[im]	=	length(all.marq[[(i-marq.hap.left)+im]])
+             freq.marq[[im]]	=	freq.a[[(i-marq.hap.left)+im]]
+             all.marq.int[[im]]	=	all.marq[[(i-marq.hap.left)+im]]
+        }# Fin FOR2
+
+
+    #calcul des structures pour l'intervalle i
+    res.structure	=	structure(marq.hap,nbre.all.marq)
+    pi.hap		=	pi.hap(freq.marq,res.structure)
+    cor.chrm1.pere	=	corresp(hap.chrom1.pere[,((i-marq.hap.left)+1):((i-marq.hap.left)+marq.hap)],res.structure)
+    cor.chrm2.pere	=	corresp(hap.chrom2.pere[,((i-marq.hap.left)+1):((i-marq.hap.left)+marq.hap)],res.structure)
+    cor.mere		=	corresp(hap.trans.mere[,((i-marq.hap.left)+1):((i-marq.hap.left)+marq.hap)],res.structure)
+    hap.assoc		=	unique(c(cor.chrm1.pere$assoc,cor.chrm2.pere$assoc,cor.mere$assoc))   
+    nbre.ass		= 	length(hap.assoc) 
+    nbre.pas		=	length(dist.test[[i]])
+
+
+        for(h in 1:nbre.pas){  # Début de la boucle sur les pas des intervalles (FOR3)
+
+             for (z in 1:length(position.new)){# début FOR4
+
+                 if (((h==1)&(position.new[z]==dist.test[[i]][h]))|((h>=2)&(position.new[z]==dist.test[[i]][h]))){# début FOR5
+
+                     i.pos	=	i.pos+1
+                     depart   	= 	depart.LDL(moyenne.pere,perf,CD,PLA[,(i.pos+i.pos.PLA)],desc.pere)
+                     i.dist	=	c(dist.marq[(i-marq.hap.left+1):i],dist.test[[i]][h],dist.marq[(i+1):(i+marq.hap.left)])
+                     poids.D	=	poids.D(i.dist,marq.hap,res.structure)
+
+
+		     # initialisation des vecteurs nécessaires dépendant du nombre d'associations
+                     ML		=	rep(NA,nbre.ass)
+
+                     if(perfectLD==TRUE){
+                         val.par	=	matrix(NA,nrow=nbre.ass,ncol=5)
+                     }
+
+                     if(perfectLD==FALSE){
+                         val.par	=	matrix(NA,nrow=nbre.ass,ncol=6)
+                     }
+
+    
+                    for (j in 1:nbre.ass){ # Début de la boucle sur toutes les associations possibles (FOR6)
+                         assoc 	= 	hap.assoc[j]
+                         don 	= 	list(perf,CD,PLA[,(i.pos+i.pos.PLA)],desc.pere,moyenne.pere,assoc,cor.chrm1.pere,cor.chrm2.pere,cor.mere,pi.hap,res.structure,poids.D)
+
+                         # recherche du minimum de la fonction obj.LDL.add (inverse de la vraisemblance)
+                         if(perfectLD==TRUE){
+                              start    	= 	c(depart,temps.depart,0.25,0)
+                              op  	= 	optim(start,obj.LDL.add,NULL,method="BFGS",lower=-Inf,upper=Inf,control=list(),hessian=FALSE,don)
+                         }
+
+                         if(perfectLD==FALSE){
+                              start    	= 	c(depart,temps.depart,0.9,0.25,0)
+                              op  	= 	optim(start,obj.LDL.add.alpha,NULL,method="BFGS",lower=-Inf,upper=Inf,control=list(),hessian=FALSE,don)
+                         }
+    
+
+                         val.par[j,]   =        op$par  
+                         ML[j]         =        -op$value
+
+                     }# Fin de la boucle sur les associations (FOR6)
+
+
+                   # Recherche de la valeur du maximum de vraisemblance  pour chaque position
+                   # sur les association possibles.
+                   # Calcul des valeurs du test et stockage des paramètres estimés
+
+                   pos.test[i.pos] 	= 	dist.test[[i]][h]
+                   LRT[i.pos]           = -2*(ML.H0- max(ML))
+                   i.val                =	which.max(ML)
+                   assoc.est[i.pos]     = 	hap.assoc[i.val]
+                   param.est[,i.pos]	=	val.par[i.val,]
+
+		   # retrouver les allèles de l'haplotype associé
+                    hap.ass.est[i.pos]=retrouve.all(assoc.est[i.pos],res.structure,all.marq.int)
+
+               } # Fin FOR5
+           } # Fin FOR4
+        }# Fin de la boucle sur les pas des intervalles (FOR3)
+     }# Fin de la boucle sur les intervalles (FOR1)
+
+
+# On met à 0 les LRT négatifs
+  LRT[LRT<0]	=	0
+
+# On regroupe les différents résultats dans un tableau
+  res		=	data.frame(pos.test,round(LRT,4),hap.ass.est,round(t(param.est),4))
+
+# On nomme les différentes colonnes du tableau
+	# cas1: perfectLD=true
+
+          if(perfectLD==TRUE){
+            dimnames(res)[[2]]	=	c("position","LRT","haplotype","variance","effect.Q","temps","pi.Q(0)","mean")
+          }
+
+	# cas2: perfectLD=false
+
+          if(perfectLD==FALSE){
+            dimnames(res)[[2]]	=	c("position","LRT","haplotype","variance","effect.Q","temps","alpha","pi.Q(0)","mean")
+          }
+
+
+res
+}
+
---
+++ R/haplomax.HS.add.R
@@ -0,0 +1,186 @@
+`haplomax.HS.add` <-
+function(hap.chrom1.pere,hap.chrom2.pere,hap.trans.pere,hap.trans.mere,perf,CD,genea,map,position,marq.hap) {
+
+# On recode les haplotypes et les genotypes
+###########################################
+	 hap.pop                =	rbind(hap.chrom1.pere,hap.chrom2.pere,hap.trans.mere)
+	 all.marq               =	allele.marq(hap.pop)
+	 hap.chrom1.pere	=	recode.hap(hap.chrom1.pere,all.marq)
+	 hap.chrom2.pere	=	recode.hap(hap.chrom2.pere,all.marq)
+	 hap.trans.mere		= 	recode.hap(hap.trans.mere,all.marq)
+	 hap.trans.pere 	= 	recode.hap(hap.trans.pere,all.marq)
+         hap.pop	        =	rbind(hap.chrom1.pere,hap.chrom2.pere,hap.trans.mere)
+
+# On récupère le nombre de pères
+################################
+	nbre.pere	=	length(hap.chrom1.pere[,1])
+
+# On calcule la distance des marques par rapport à l'origine
+############################################################
+	 dist.marq	=	distance.marqueurs(map)
+
+# On calcule le nombre d'intervalles entre les marques
+######################################################
+	 nbre.int	=	length(map) 
+
+# On calcule du nombre de positions de tests
+############################################
+	  num.pos	= 	nbre.int-marq.hap+2
+
+# On calcule le nombre d'allèles de l'haplotype reconstruit à gauche de la position de test
+############################################################################################
+          marq.hap.left  =       marq.hap/2
+
+#Contrôle sur les positions de tests et redimensionnement du vecteur de positions de tests en fonction des positions cohérentes
+###############################################################################################################################
+# 1ère vérification: les positions rentrées par l'utilisateur doivent être comprises entre la 1ère marque et la dernière marque
+# Les positions sont dans position.new
+         position=sort(position) #rangement des valeurs de position par ordre croissant
+         position=round(position,5) #Il faut arrondir!
+
+         if (marq.hap.left==1) {dist.marq1=dist.marq}
+         if (marq.hap.left>1) {dist.marq1=dist.marq[-c(1:(marq.hap.left-1),(length(dist.marq)-marq.hap.left+2):length(dist.marq))]}
+
+         borne.inf=round(dist.marq1[1],5) #Il faut arrondir!
+         borne.sup=round(dist.marq1[length(dist.marq1)],5) #Il faut arrondir!
+
+
+         diff.left=position-borne.inf 
+         diff.left
+         diff.right=position-borne.sup 
+         diff.right
+
+         which=(1:length(position))[(diff.left>=0)&(diff.right<=0)]
+         position.new=round(sort(position[which]),5)
+         position.new
+
+         if ((length(position.new)-marq.hap.left+1)!=length(position)) stop("error in test positions",call.=FALSE) 
+
+
+#initialisation des vecteurs résultats
+######################################
+	 Fisher		= 	rep(NA,num.pos)
+	 pos.test	=	rep(NA,num.pos)
+	 assoc.est	= 	rep(NA,num.pos) 
+	 hap.ass.est 	= 	rep(NA,num.pos)
+	 param.est 	=	 matrix(NA,ncol=num.pos,nrow=2)
+
+
+for (i in 1:num.pos)	
+
+{ # Début de la boucle sur les positions de tests
+
+#extraction des informations pour la position i
+nbre.all.marq	=	rep(0,marq.hap)
+all.marq.int	=	list()
+
+for(im in 1:(marq.hap)){
+  nbre.all.marq[im]	=	length(all.marq[[(i+im-1)]])
+  all.marq.int[[im]]	=	all.marq[[(i+im-1)]]
+}
+
+#calcul des structures pour la position i
+  res.structure	=	structure(marq.hap,nbre.all.marq)
+  cor.hap	=	corresp(hap.pop[,i:(i+marq.hap-1)],res.structure)
+  pi.hap	=	pi.hap.NI(res.structure,cor.hap)
+  cor.pere	=	corresp(hap.trans.pere[,i:(i+marq.hap-1)],res.structure)
+  cor.mere	=	corresp(hap.trans.mere[,i:(i+marq.hap-1)],res.structure)  
+  hap.assoc	=	unique(c(cor.pere$assoc,cor.mere$assoc))   
+  nbre.ass	= 	length(hap.assoc) 
+
+# initialisation des vecteurs nécessaires dépendant du nombre d'associations
+  F.assoc		=	rep(NA,nbre.ass)
+  val.par		=	matrix(NA,nrow=nbre.ass,ncol=2)
+
+  if(nbre.ass>1){ # début du if sur nbre.ass
+   for (j in 1:nbre.ass) {  # Début de la boucle sur toutes les associations possibles
+   assoc 	= 	hap.assoc[j]  
+   res		=	obj.haplomax.HS.add(genea,perf,CD,assoc,res.structure,pi.hap,cor.pere,cor.mere)
+   sum.res	=	summary(res)
+   temp		=	sum.res[[1]]
+   F.assoc[j]	=	temp$F[2]
+   val.par[j,2]	=	res$coefficient[nbre.pere+1]
+   val.par[j,1]	=	temp$Mean[length(temp$Mean)]
+   }   # Fin de la boucle sur les associations
+
+  i.val	=	which.max(F.assoc)
+
+    if (!is.na(i.val)){
+     assoc.est[i]	=	hap.assoc[i.val]
+     Fisher[i]		=	F.assoc[i.val]
+     param.est[,i]	=	val.par[i.val,]
+
+     #retrouver les allèles de l'haplotype associé
+     hap.ass.est[i]	=	retrouve.all(assoc.est[i],res.structure,all.marq.int)
+     }
+
+   }  # fin du if sur nbre.assoc
+
+} # fin de la boucle sur les positions de tests
+
+#On récupère les positions de test
+###################################
+    nbre.marq	 = 	length(dist.marq)
+    pos		 =	dist.marq[1:(nbre.marq-marq.hap+1)]
+
+#Regroupement des résultats sous forme de tableau
+#################################################
+    res	         =	data.frame(pos,Fisher,hap.ass.est,t(param.est)) 
+
+
+# initialisation des objets allant recevoir les résultats
+#########################################################
+
+    res2	=	data.frame(c1=rep(NA,length(position.new)),c2=rep(NA,length(position.new)),c3=rep(NA,length(position.new)),c4=rep(NA,length(position.new)),c5=rep(NA,length(position.new)))
+
+   Fisher2	=	rep(NA,length(position.new))
+
+   variance2	=	rep(NA,length(position.new))
+
+   effet.Q2	=	rep(NA,length(position.new))
+
+
+for (i in 1:(length(pos)-1)){ # début de la boucle sur i
+   for (j in 1:length(position.new)){ # début de la boucle sur j
+
+   #cas où la position de test (position.new[j]) est comprise entre la borne sup (pos[i+1]) et la borne inf (pos[i])de l'intervalle
+   #On recalcule le test de Fisher, la variance et l'effet du QTL en pondérant par rapport à la borne sup et borne inf de l'intervalle
+     if ((pos[i]<position.new[j])&(position.new[j]<pos[i+1])) {
+       Fisher2[j]       =      ((pos[i+1]-position.new[j])/(pos[i+1]-pos[i]))*res[i+1,2]    +   ((position.new[j]-pos[i])/(pos[i+1]-pos[i]))*res[i,2]
+       variance2[j]     =      ((pos[i+1]-position.new[j])/(pos[i+1]-pos[i]))*res[i+1,4]    +   ((position.new[j]-pos[i])/(pos[i+1]-pos[i]))*res[i,4]
+       effet.Q2[j]      =      ((pos[i+1]-position.new[j])/(pos[i+1]-pos[i]))*res[i+1,5]    +   ((position.new[j]-pos[i])/(pos[i+1]-pos[i]))*res[i,5]
+       res2[j,]		=	cbind(position.new[j],round(Fisher2[j],4),paste(res[i,3]),round(variance2[j],4),round(effet.Q2[j],4))
+      }
+
+   #cas où la position de test (position.new[j]) est égale à la position d'une marque (pos[i])
+   #Les valeurs des tests de Fisher, de la variance et de l'effet du QTL ne changent pas
+     if (pos[i]==position.new[j]) {
+       Fisher2[j]       =      res[i,2]
+       variance2[j]     =      res[i,4]
+       effet.Q2[j]      =      res[i,5]
+       res2[j,]		=      cbind(position.new[j],round(Fisher2[j],4),paste(res[i,3]),round(variance2[j],4),round(effet.Q2[j],4))
+      }
+
+   #cas où la position est supérieure à la position de l'avant dernière marque
+   #On reprend les valeurs des tests de Fisher, de la variance et de l'effet du QTL de l'avant dernière marque
+     if (((pos[length(pos)]<position.new[j])&(dist.marq[length(dist.marq)]>=position.new[j]))||(pos[length(pos)]==position.new[j])){
+       Fisher2[j]       =      res[length(pos),2]
+       variance2[j]     =      res[length(pos),4]
+       effet.Q2[j]      =      res[length(pos),5]
+       res2[j,]		=      cbind(position.new[j],round(Fisher2[j],4),paste(res[length(pos),3]),round(variance2[j],4),round(effet.Q2[j],4))
+      }
+   
+    }# début de la boucle sur j
+
+ } # fin de la boucle sur i
+
+
+#On nomme les colonnes du data.frame
+dimnames(res2)[[2]]=c("position","F","haplotype","variance","effect.Q")
+
+res2
+
+
+
+}
+
